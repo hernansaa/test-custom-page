c6948cde6a50fd423dffae4ebb5ad95261a9440f
hernansaa <hernansaa88@gmail.com>
2024-08-18 12:11:53 +0200

    add gs color patter to unfold admin

--- gs_admin/urls.py
+++ gs_admin/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .sites import new_admin_site
 
 urlpatterns = [
-    path("unfold-admin/", new_admin_site.urls), # <-- Unfold admin
+    path("gs-admin/", new_admin_site.urls), # <-- Unfold admin
 ] 
